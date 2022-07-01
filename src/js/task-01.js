e2924f4e0ebd310c6251a1c2d2c6a9f24e1ec409
MarynaPyvovar <marina18renkas@gmail.com>
2022-07-01 18:56:55 +0300

    task-02

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -2,8 +2,8 @@ const categoriesEl = document.querySelectorAll('li.item');
 
 console.log(`Number of categories: ${categoriesEl.length}`);
 
-const elementsEl = (array = categoriesEl) => {
-    [...categoriesEl].map(category => {
+const elementsEl = () => {
+    categoriesEl.forEach(category => {
             const categoryNameEl = category.firstElementChild.textContent;
             const elementsQuantity = category.lastElementChild.children.length;
 
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsListEl = document.querySelector('#ingredients');
+
+const addElements = () => {
+  const ingredientsList = ingredients.map(ingredient => {
+    const elementsEl = document.createElement('li');
+    
+    elementsEl.textContent = ingredient;
+    elementsEl.classList.add('item');
+
+    return elementsEl;
+  })
+
+  ingredientsListEl.append(...ingredientsList)
+}
+
+addElements()
