2a546d3113b271465a0c41b7e0e3b542d9e860df
MarynaPyvovar <marina18renkas@gmail.com>
2022-07-05 13:33:52 +0300

    task-09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  changeColorBtn: document.querySelector('.change-color'),
+  textColorEl: document.querySelector('.color'),
+  bodyEl: document.querySelector('body'),
+}
+
+refs.changeColorBtn.addEventListener('click', () => {
+  refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  refs.textColorEl.textContent = getRandomHexColor();
+})
